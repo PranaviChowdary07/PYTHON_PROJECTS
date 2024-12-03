9b351fabf2a2fce6b95d836231013e3af3783714
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-12-03 12:57:17 +0530

    game over

--- 02_Snake_Game.py
+++ 02_Snake_Game.py
@@ -41,11 +41,14 @@ food = Tile(10*TILE_SIZE, 10*TILE_SIZE)
 snake_body = []  # multiple body tiles
 velocityX = 0
 velocityY = 0
+game_over = False
 
 def change_direction(e): # e = event
     #print(e)
     #print(e.keysym) for directions like left,right,up,down
-    global velocityX, velocityY
+    global velocityX, velocityY, game_over
+    if (game_over):
+        return
 
     if (e.keysym == "Up" and velocityY != 1):
         velocityX = 0
@@ -60,13 +63,35 @@ def change_direction(e): # e = event
         velocityX = 1
         velocityY = 0
 def move():
-    global snake 
+    global snake ,food, snake_body, game_over
+    if (game_over):
+        return
+    if(snake.x < 0 or snake.x >= WINDOW_WIDTH or snake.y < 0 or snake.y>= WINDOW_HEIGHT):
+        game_over = True
+        return
+
+    for  tile in snake_body:
+        if (snake.x == tile.x and  snake.y == tile.y):
+            game_over = True
+            return
 
     # collision 
     if (snake.x == food.x and snake.y == food.y):
         snake_body.append(Tile(food.x, food.y))
         food.x = random.randint(0, COLS-1) * TILE_SIZE
         food.y = random.randint(0,ROWS-1)
+
+    # update snake body
+    for i in range(len(snake_body)-1, -1, -1):
+        tile = snake_body[i]
+        if (i == 0):
+            tile.x = snake.x
+            tile.y = snake.y
+        else:
+            prev_tile = snake_body[i-1]
+            tile.x = prev_tile.x
+            tile.y = prev_tile.y
+
 
     snake.x +=  velocityX * TILE_SIZE
     snake.y += velocityY * TILE_SIZE

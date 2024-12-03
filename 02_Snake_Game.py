623a0705b3441bba5ea2d696e2e93de51c90cc9f
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-12-03 11:27:33 +0530

    Set the block

--- 02_Snake_Game.py
+++ 02_Snake_Game.py
@@ -10,12 +10,23 @@ WINDOW_HEIGHT = TILE_SIZE * COLS
  
  # game window
 window = tkinter.Tk()
-window.title("Snake")
+window.title("SNAKE")
 window.resizable(False,False)
 
-canvas = tkinter.Canvas(window,bg = "black", width = WINDOW_WIDTH, height = WINDOW_HEIGHT)
+canvas = tkinter.Canvas(window,bg = "black", width = WINDOW_WIDTH, height = WINDOW_HEIGHT,borderwidth = 0,highlightthickness = 0)
 canvas.pack()
 window.update()
+
+# Center window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+# format = (width)x (height) + x + y
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 
 window.mainloop()

b241781e95c5d0d203681df61dad27176b2ccb4f
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-12-03 11:09:43 +0530

    Addn sizes

--- 02_Snake_Game.py
+++ 02_Snake_Game.py
@@ -7,4 +7,17 @@ TILE_SIZE = 25
 
 WINDOW_WIDTH = TILE_SIZE * ROWS
 WINDOW_HEIGHT = TILE_SIZE * COLS
+ 
+ # game window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False,False)
+
+canvas = tkinter.Canvas(window,bg = "black", width = WINDOW_WIDTH, height = WINDOW_HEIGHT)
+canvas.pack()
+window.update()
+
+
+window.mainloop()
+
 

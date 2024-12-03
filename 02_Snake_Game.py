66718469ec98d727e7d4869469fa00e01cb32610
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-12-03 11:57:34 +0530

    Add Directions

--- 02_Snake_Game.py
+++ 02_Snake_Game.py
@@ -37,15 +37,40 @@ window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 # initialize the game
 snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) # single tile,snakes's head
+food = Tile(10*TILE_SIZE, 10*TILE_SIZE)
+velocityX = 0
+velocityY = 0
 
+def change_direction(e): # e = event
+    #print(e)
+    #print(e.keysym) for directions like left,right,up,down
+    global velocityX, velocityY
+
+    if (e.keysym == "Up"):
+        velocityX = 0
+        velocityY = -1
+    elif (e.keysym == "Down"):
+        velocityX = 0
+        velocityY = 1
+    elif (e.keysym == "Left"):
+        velocityX = -1
+        velocityY = 0
+    elif (e.keysym == "Right"):
+        velocityX = 1
+        velocityY = 0
+        
+   
 def draw():
     global snake
 
     # draw snake
     canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green")
+    # draw food
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y+ TILE_SIZE, fill = "red")
     window.after(100,draw)  # 100ms = 1/10 sec , 10 frames/sec
 
 draw() 
+window.bind("<KeyRelease>", change_direction)
 window.mainloop()
 
 
